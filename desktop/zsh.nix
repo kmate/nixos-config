9404ce1ef1b8dea8969a58e340bfdf974d9a228b
kmate <mtkarc@gmail.com>
2024-03-23 21:16:11 +0100

    Make zsh default for nix-shell

--- desktop/zsh.nix
+++ desktop/zsh.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   programs.zsh = {
     enable = true;
     oh-my-zsh = {
@@ -12,5 +12,18 @@
     enableAutosuggestions = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
+
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
   };
 }
