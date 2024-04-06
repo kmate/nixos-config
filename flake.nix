8ee5d48fd01e3308a8540a8f7c481c2b4f78bd30
kmate <mtkarc@gmail.com>
2024-04-06 15:07:44 +0200

    Extract locale settings to a file

--- flake.nix
+++ flake.nix
@@ -69,32 +69,12 @@
           ./secrets.nix
           ./network.nix
           ./utils.nix
+          ./locale.nix
           ./users.nix
           ./fingerprint.nix
           ./fonts.nix
           ./desktop.nix
           ./platformio.nix
-          {
-            time.timeZone = "Europe/Budapest";
-            i18n = {
-              defaultLocale = "en_US.UTF-8";
-              supportedLocales = [
-                "en_US.UTF-8/UTF-8"
-                "hu_HU.UTF-8/UTF-8"
-              ];
-              extraLocaleSettings = {
-                LC_ADDRESS = "hu_HU.UTF-8";
-                LC_IDENTIFICATION = "hu_HU.UTF-8";
-                LC_MEASUREMENT = "hu_HU.UTF-8";
-                LC_MONETARY = "hu_HU.UTF-8";
-                LC_NAME = "hu_HU.UTF-8";
-                LC_NUMERIC = "hu_HU.UTF-8";
-                LC_PAPER = "hu_HU.UTF-8";
-                LC_TELEPHONE = "hu_HU.UTF-8";
-                LC_TIME = "hu_HU.UTF-8";
-              };
-            };
-          }
           home-manager.nixosModules.home-manager
           {
             home-manager = {
