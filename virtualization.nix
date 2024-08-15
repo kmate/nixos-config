2e0a73a195d5161097583fec31d157bd4f648ed2
kmate <mtkarc@gmail.com>
2024-08-15 16:35:05 +0200

    Re-enable virtualization

--- virtualization.nix
+++ virtualization.nix
@@ -12,14 +12,13 @@
       defaultNetwork.settings.dns_enabled = true;
     };
 
-    # TODO: re-enable after https://github.com/NixOS/nixpkgs/pull/318330 gets merged
-    #virtualbox = {
-    #  host.enable = true;
-    #  guest.enable = true;
-    #};
+    virtualbox = {
+      host.enable = true;
+      guest.enable = true;
+    };
   };
 
-  #users.extraGroups = {
-  #  vboxusers.members = ["km"];
-  #};
+  users.extraGroups = {
+    vboxusers.members = ["km"];
+  };
 }
