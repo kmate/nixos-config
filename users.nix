31be90553b44ee8f37106c0979ae0bf9ba21a400
kmate <mtkarc@gmail.com>
2024-09-02 22:35:14 +0200

    Add serial usb access

--- users.nix
+++ users.nix
@@ -8,7 +8,7 @@
 
     users.km = {
       isNormalUser = true;
-      extraGroups = ["wheel" "networkmanager" "video"];
+      extraGroups = ["wheel" "dialout" "networkmanager" "video"];
       packages = with pkgs; [
         google-chrome
       ];
