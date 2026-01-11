4f690e9fea44e8ee9271d28c45caf43f3ae3d6d9
kmate <mtkarc@gmail.com>
2026-01-11 16:01:08 +0100

    Remove old hyprland configs

--- desktop/hyprland.nix
+++ desktop/hyprland.nix
@@ -54,10 +54,10 @@
         sensitivity = -0.5
       }
 
+      gesture = 3, horizontal, workspace
+
       gestures {
-        workspace_swipe = true
         workspace_swipe_invert = false
-        workspace_swipe_fingers = 3
         workspace_swipe_distance = 500
         workspace_swipe_cancel_ratio = 0.5
         workspace_swipe_min_speed_to_force = 10
