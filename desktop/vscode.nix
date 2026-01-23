cf5cd1c06c705772bf9c503d7855dc72cdafec76
kmate <mtkarc@gmail.com>
2026-01-23 22:58:34 +0100

    Add zig support

--- desktop/vscode.nix
+++ desktop/vscode.nix
@@ -4,6 +4,9 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
+    extensions = with pkgs.vscode-extensions; [
+      ziglang.vscode-zig
+    ];
     profiles.default.userSettings = {
       "files.trimFinalNewlines" = true;
       "files.insertFinalNewline" = true;
--- home.nix
+++ home.nix
@@ -81,6 +81,10 @@
         vlc
         wine
         winetricks
+        zig
+        zig-zlint
+        zls
+        zon2nix
 
         hyprpicker
         kitty
