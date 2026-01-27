88c3d2ba9097d6e40d4f54fe369a7f55ea5ddd17
kmate <mtkarc@gmail.com>
2026-01-27 11:32:42 +0100

    Use profile settings for vscode

--- desktop/vscode.nix
+++ desktop/vscode.nix
@@ -1,18 +1,18 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
-    extensions = with pkgs.vscode-extensions; [
-      ziglang.vscode-zig
-    ];
-    profiles.default.userSettings = {
-      "files.trimFinalNewlines" = true;
-      "files.insertFinalNewline" = true;
-      "explorer.confirmDelete" = false;
-      "platformio-ide.useBuiltinPIOCore" = false;
-      "update.mode" = "none";
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        ziglang.vscode-zig
+      ];
+      userSettings = {
+        "files.trimFinalNewlines" = true;
+        "files.insertFinalNewline" = true;
+        "explorer.confirmDelete" = false;
+        "platformio-ide.useBuiltinPIOCore" = false;
+        "update.mode" = "none";
+      };
     };
   };
 }
